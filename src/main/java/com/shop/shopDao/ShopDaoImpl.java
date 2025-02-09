75ee19e0590faa9ec4ec51448ab9b8da510e4170
Ruhul Amin <166214022+ruhul7488@users.noreply.github.com>
2025-02-09 12:25:38 +0530

    Update ShopDaoImpl.java

--- src/main/java/com/shop/shopDao/ShopDaoImpl.java
+++ src/main/java/com/shop/shopDao/ShopDaoImpl.java
@@ -23,20 +23,7 @@ public  class ShopDaoImpl implements ShopDAO{
 		
     EntityManager em = emf.createEntityManager() ;
 
-    //  public List<Product> getproductDetailByname(String productModel) throws SQLException{
-             
-                
-    //                    String sql = "SELECT * FROM product WHERE productModel = :productModel";
-               
-    //                    Query query = em.createNativeQuery(sql, Product.class);
-               
-    //                    query.setParameter("productModel",productModel);
-               
-    //                   List<Product> product = query.getResultList();
-               
-    //                    return product;
-               
-    //            }
+   
 
    public List<Product> getproductDetailByname(String productModel) {
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("ShopUnit");
@@ -59,20 +46,7 @@ public  class ShopDaoImpl implements ShopDAO{
 
 
 
-            //    public List<Product> getAllProduct() throws SQLException {
-            //     try {
-
-            //         String sql = "SELECT * FROM product";
-            //         Query query = em.createNativeQuery(sql, Product.class);
-        
-            //         List<Product> productList = query.getResultList();
-        
-            //         return productList;
-        
-            //     } catch (Exception e) {
-            //         throw new SQLException("Error fetching all products", e);
-            //     }
-            // }
+            
             public List<Product> getAllProduct() {
                 EntityManagerFactory emf = Persistence.createEntityManagerFactory("ShopUnit");
                 EntityManager em = emf.createEntityManager();
@@ -92,16 +66,7 @@ public  class ShopDaoImpl implements ShopDAO{
             }
             
 
-        //   public  void newStock(String productModel, String brand, String serialnumber, int weight, String color, String display, String storage, String operatin_system, String ram, int warranty, double actual_price,double saling_price)throws SQLException{
-
-        //     Stocks st=new Stocks(productModel, brand, serialnumber, weight, color, display, storage, operatin_system, ram, warranty, actual_price, saling_price);
-
-        //     em.getTransaction().begin();
-        //     em.persist(st);
-        //     em.getTransaction().commit();
-        //     em.close();
-
-        //   }
+      
 
         public void newStock(String productModel, String brand, String serialNumber, double  weight, 
                      String color, String display, String storage, String operatingSystem, 
@@ -128,15 +93,7 @@ public  class ShopDaoImpl implements ShopDAO{
 }
 
 
-        //  public void addCustomer(String customer_name, long phone, String email, Address address)throws SQLException{
-
-        //     Customer cs=new Customer(customer_name, phone, email, address);
-        //     em.getTransaction().begin();
-        //     em.persist(cs);
-        //     em.getTransaction().commit();
-        //     em.close();
-
-        //   }
+       
 
         public void addCustomer(String customerName, long phone, String email, Address address) {
             EntityManager em = emf.createEntityManager();  
@@ -159,19 +116,7 @@ public  class ShopDaoImpl implements ShopDAO{
             }
         }
         
-        //  public Customer viewCustomerByphone(long phone)throws SQLException{
-             
-        //     String qr1="select * from Customer where phone=:phone";
-
-        //     Query query1 = em.createNativeQuery(qr1, Customer.class);
-               
-        //                query1.setParameter("phone",phone);
-               
-        //                Customer cst = (Customer) query1.getSingleResult();
-               
-        //                return cst;
-           
-        //  }
+      
 
         public Customer viewCustomerByphone(long phone) {
     EntityManager em = emf.createEntityManager();  
@@ -199,21 +144,7 @@ public  class ShopDaoImpl implements ShopDAO{
 
 
 
-        //  public List<Customer> viewAllCustomer()throws SQLException {
-          
-        //     try {
-
-        //         String sql = "SELECT * FROM Customer";
-        //         Query query = em.createNativeQuery(sql, Customer.class);
-    
-        //         List<Customer> customerList = query.getResultList();
-    
-        //         return customerList;
-    
-        //     } catch (Exception e) {
-        //         throw new SQLException("Error fetching all products", e);
-        //     }
-        //  }
+     
 
         public List<Customer> viewAllCustomer() {
             EntityManager em = emf.createEntityManager();  
@@ -234,18 +165,7 @@ public  class ShopDaoImpl implements ShopDAO{
         }
         
 
-        //  public Stocks getStockbyname(String productModel)throws SQLException{
-                
-        //     String qr1="select * from Stocks where productModel=:productModel";
-
-        //     Query query1 = em.createNativeQuery(qr1, Stocks.class);
-               
-        //                query1.setParameter("productModel",productModel);
-               
-        //                Stocks sct = (Stocks) query1.getSingleResult();
-               
-        //                return sct;
-        //  }
+       
 
         public List<Stocks>getStockbyname(String productModel) {
             EntityManager em = emf.createEntityManager();  
@@ -271,21 +191,7 @@ public  class ShopDaoImpl implements ShopDAO{
         }
         
 
-        //  public List<Stocks> getAllStocks()throws SQLException{
-
-        //     try {
-
-        //         String sql = "SELECT * FROM Stocks";
-        //         Query query = em.createNativeQuery(sql, Stocks.class);
-    
-        //         List<Stocks> stockList = query.getResultList();
-    
-        //         return stockList;
-    
-        //     } catch (Exception e) {
-        //         throw new SQLException("Error fetching all products", e);
-        //     }
-        //  }
+      
 
         public List<Stocks> getAllStocks() {
             EntityManager em = emf.createEntityManager();  
@@ -307,16 +213,7 @@ public  class ShopDaoImpl implements ShopDAO{
         }
         
           
-        //  public void generateBill(String customer_name, long phone, String email, Address address, String invoice_number,Date billiDate, String typesofPayment, String productModel, String brand, String serialnumber, int weight,String color, String display, String storage, String operatin_system, String ram, int warranty,double amount) throws SQLException{
-
-        //     Bill bil =new Bill(customer_name, phone, email, address, invoice_number, billiDate, typesofPayment, productModel, brand, serialnumber, weight, color, display, storage, operatin_system, ram, warranty, amount);
-
-        //     em.getTransaction().begin();
-        //     em.persist(bil);
-        //     em.getTransaction().commit();
-        //     em.close();
-
-        //  }
+      
 
         public void generateBill(String customerName, long phone, String email, Address address, 
                          long invoiceNumber, Date billDate, String typesOfPayment, 
@@ -345,19 +242,7 @@ public  class ShopDaoImpl implements ShopDAO{
     }
 }
 
-            
-        //   public Bill getbillByphone(long phone)throws SQLException{
-
-        //     String qr1="select * from Bill where phone=:phone";
-
-        //     Query query1 = em.createNativeQuery(qr1, Bill.class);
-               
-        //                query1.setParameter("phone",phone);
-               
-        //                Bill bil = (Bill) query1.getSingleResult();
-               
-        //                return bil;
-        //   }
+       
 
         public Bill getbillByphone(long phone) {
             EntityManager em = emf.createEntityManager();  
@@ -384,21 +269,7 @@ public  class ShopDaoImpl implements ShopDAO{
         
 
                
-        // public  List <Bill> getAllBills() throws SQLException{
-
-        //     try {
-
-        //         String sql = "SELECT * FROM Bill";
-        //         Query query = em.createNativeQuery(sql, Bill.class);
-    
-        //         List<Bill> billList = query.getResultList();
-    
-        //         return billList;
-    
-        //     } catch (Exception e) {
-        //         throw new SQLException("Error fetching all products", e);
-        //     }
-        // }
+        
 
           
         public List<Bill> getAllBills() {
